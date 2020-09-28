0fc346e947ac56d94cad78e342a2a1de84e1e0cb
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-28 02:54:20 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,14 @@
 class Cat
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+  
+  def initialize(name, owner)
+    @name = name
+    @mood = nervous
+    @owner = owner
+  end
+    
+  def mood
+    @mood
+  end
 end
