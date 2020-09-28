5d4575b7251f331578bcfe86d5b7c35a2bc9ae7c
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-28 02:54:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,18 @@
 class Dog
-  attr_accessor :mood
+  attr_accessor :mood, :owner
   attr_reader :name
 
-  def initialize(name)
+@@all[]
+
+  def initialize(name, owner)
     @name = name
+    @owner = owner
+    @owner.dog << self 
     @mood = "nervous"
+    @all << self
   end
+  
+  def self.all
+    @@all
+  end 
 end
