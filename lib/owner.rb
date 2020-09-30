53222b2cb60e2830bd5436dfb3b78e278a029cd8
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-30 06:59:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,7 +5,7 @@ class Owner
 @@all = []
 @@pets = {:fishes => [], :dogs => [], :cats => []}
 
-   def initialize(species)
+  def initialize(species)
     @species = species
     @name = name
     @@all << self
