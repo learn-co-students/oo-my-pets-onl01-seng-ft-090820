13041a1a170de162e3ad26b1c43d89b19bc581c7
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-30 06:56:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,7 +3,7 @@ class Owner
   attr_reader :species, :name
 
  @@all = []
-  @@pets = {:fishes => [], :dogs => [], :cats => []}
+@@pets = {:fishes => [], :dogs => [], :cats => []}
 
   def initialize(name)
     @name = name
